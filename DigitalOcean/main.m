d447fcbedd50e1512bd7bc761d6f10f3cfa42c13
Axel Rivera <axel@riveralabs.com>
2013-07-13 00:44:01 -0400

    Initial Commit

---
+++ DigitalOcean/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DigitalOcean
+//
+//  Created by Axel Rivera on 7/13/13.
+//  Copyright (c) 2013 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DigitalOceanTests/DigitalOceanTests.m
@@ -0,0 +1,36 @@
+//
+//  DigitalOceanTests.m
+//  DigitalOceanTests
+//
+//  Created by Axel Rivera on 7/13/13.
+//  Copyright (c) 2013 Axel Rivera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DigitalOceanTests : XCTestCase
+
+@end
+
+@implementation DigitalOceanTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
